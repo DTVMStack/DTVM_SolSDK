e412f93225ed87a6352ed63917323112c23bb9a3
weicz11 <changzheng.wcz@antgroup.com>
2025-04-21 21:25:54 +0800

    add parsed ast structure

---
+++ src/yul2ir/errors.rs
@@ -0,0 +1,16 @@
+// Copyright (C) 2024-2025 Ant Group Co., Ltd. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+#[derive(Debug, Clone, Default)]
+pub struct ASTLoweringError {
+    #[allow(unused)]
+    pub message: String,
+}
+
+impl From<inkwell::builder::BuilderError> for ASTLoweringError {
+    fn from(err: inkwell::builder::BuilderError) -> ASTLoweringError {
+        ASTLoweringError {
+            message: err.to_string(), // Convert the error message to a string
+        }
+    }
+}
---
+++ src/yul2ir/mod.rs
@@ -0,0 +1,19 @@
+// Copyright (C) 2024-2025 Ant Group Co., Ltd. All Rights Reserved.
+// SPDX-License-Identifier: Apache-2.0
+
+use lalrpop_util::lalrpop_mod;
+lalrpop_mod!(pub yul); // synthesized by LALRPOP
+
+pub mod ast;
+pub mod config;
+pub mod context;
+pub mod errors;
+pub mod infer;
+pub mod instruction;
+pub mod stdlib;
+pub mod transform;
+pub mod usage;
+pub mod utils;
+pub mod var_scope;
+pub mod wasm;
+pub mod yul_instruction;
